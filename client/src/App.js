54a0956b3b16f6e24683dd2bbe4fb3bc2d271050
MarcoDayz <diaz.marco8686@gmail.com>
2022-10-15 19:10:43 -0700

    added failed screen component

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,19 @@ import InputBox from "./components/InputBox";
 import RestartButton from "./components/RestartButton";
 import NextButton from "./components/NextButton";
 import WPMSpeed from "./components/WPMSpeed";
+import FailedScreen from "./components/FailedScreen";
 
 const App = () => {
     const getQuotesURL = 'http://localhost:4000/quotes';
 
     const [loading, setLoading] = useState(true);
-    const [quotes, setQuotes] = useState('test');
+    const [quotes, setQuotes] = useState('');
     const [userInput, setUserInput] = useState('');
     let [seconds, setSeconds] = useState(0);
     const [correctSymbols, setCorrectSymbols] = useState(0);
     const [started, setStarted] = useState(false);
     const [finished, setFinished] = useState(false);
+    const [incomplete, setIncomplete] = useState(false)
 
     useEffect(() => {
         const getQuotes = async () => {
@@ -44,7 +46,6 @@ const App = () => {
         setCorrectSymbols(0)
         setStarted(false)
         setFinished(false)
-        // setLoading(true)
     }
 
     const handleNext = () => {
@@ -60,7 +61,11 @@ const App = () => {
     }
 
     const handleFinished = (inputValue) => {
-        if(quotes === inputValue || seconds === 60){
+        if(quotes.length > inputValue.length && seconds === 60){
+            setIncomplete(true)
+            setFinished(true)
+        }
+        if(quotes === inputValue && seconds <= 60 ){
             setFinished(true)
             setStarted(false)
         }
@@ -84,29 +89,40 @@ useEffect(()=> {
         const correctSymbol = inputValue.replace(' ', '').split('').filter((char,index) => char === text[index]).length;
         setCorrectSymbols(correctSymbol)
     };
-
-    if(!loading){
-    return (
-        <div className="mainContainer">
-            <div className="previewContainer">
-                <Preview quotes={quotes} userInput={userInput}/>
-            </div>
-            <div>
-                <InputBox userInput={userInput} handleInputOnChange={handleInputOnChange} finished={finished}/>                
-            </div>
-            <div>
-                <RestartButton handleRestart={handleRestart}/>
-                <NextButton handleNext={handleNext}/>
-            </div>
-            <div>
+    if(started && incomplete){
+        return (
+            <div className="mainContainer">
+                <div className="FailedContainer">
+                    <FailedScreen />                    
+                </div>
                 <WPMSpeed seconds={seconds} correctSymbols={correctSymbols} userInput={userInput}/>
+                <RestartButton handleRestart={handleRestart}/>
+            </div>  
+        )
+    }else if(!loading){
+        return (
+            <div className="mainContainer">
+                <div className="previewContainer">
+                    <Preview quotes={quotes} userInput={userInput}/>
+                </div>
+                <div>
+                    <InputBox userInput={userInput} handleInputOnChange={handleInputOnChange} finished={finished}/>                
+                </div>
+                <div>
+                    <RestartButton handleRestart={handleRestart}/>
+                    <NextButton handleNext={handleNext}/>
+                </div>
+                <div>
+                    <WPMSpeed seconds={seconds} correctSymbols={correctSymbols} userInput={userInput}/>
+                </div>
             </div>
-        </div>
-    )
+        )
     }else{
-        return <div>
-            <h1>Loading...</h1>
-        </div>
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
     }
 }
 
---
+++ client/src/components/FailedScreen.js
@@ -0,0 +1,9 @@
+const FailedScreen = () => {
+
+    return <h1>Time ran out! 
+        <br/>
+        Try again!
+    </h1>
+}
+
+export default FailedScreen;
