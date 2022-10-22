297dd21a2feb0f713ff81cf44ecfd7a7df243e4f
MarcoDayz <diaz.marco8686@gmail.com>
2022-10-22 15:48:33 -0700

    prep for deployment

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
-//import pool from "./createPool.js"
+import {} from "dotenv/config"
 import pool from "./localPool.js";
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static("public"))
 
 app.get('/quotes', async (req, res) => {
     try {
@@ -22,7 +23,7 @@ app.get('/quotes', async (req, res) => {
 app.get('/quotes/:id', async (req, res) => {
     try {
         const {id} = req.params;
-        const {rows} = await pool.query('SELECT quote FROM quotes WHERE id = $1;', [id]);
+        const {rows} = await pool.query('SELECT * FROM quotes WHERE id = $1;', [id]);
         res.contentType('application/json').status(200)
         res.send(rows)
     } catch (error) {
